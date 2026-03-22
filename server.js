69f8c5ce7af11e82d53e710c4ed85eba43b11349
saksham762007 <samrathore07062007@gmail.com>
2026-03-22 22:21:43 +0530

    file update

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const express = require("express");
 const connectDB = require("./src/config/db");
 const app = express();
 
-// database connection
-connectDB();
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credential: true
+}));
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.use(express.json({limit: "20kb"}));   // json format mai data lana ke liye 
+app.use(express.urlencoded());    //URL se data lane ke liye
+app.use(express.static("public"));   //PDF ya koi folder store krne ke liye
+
+
+// database connection
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running on port : ${process.env.PORT || 8000}`);   //then or catch ka use krna imp. hai kyu ki kabhi kabhi server nhi chalta to pata chalna chaiye 
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed !!!", err);
 });
+
