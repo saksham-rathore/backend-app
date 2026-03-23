6426c8d5a0fa64fe85951dd819a87567473a0714
saksham762007 <samrathore07062007@gmail.com>
2026-03-23 23:23:28 +0530

    file update

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -4,6 +4,35 @@ const VideoSchema = new Schema(
     {
     videofile: {
         type: String, //cloudinary URL
+        required: true,
+    },
+    thumbnail: {
+        type: String,  //cloudinary URL
+        required: True,
+    },
+    title: {
+        type: String, 
+        required: True,
+    },
+    Description: {
+        type: String, 
+        required: True,
+    },
+    Duration: {
+        type: Number, 
+        required: True,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,
+        default: true,
+    },
+    Owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
     }
 },
 {
