08732c197cf2bb412324c85c70f7b6c3c22bcbfb
saksham762007 <samrathore07062007@gmail.com>
2026-03-24 16:33:39 +0530

    file update

---
+++ utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadImage = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const result = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("Uploaded:", result.url);
+    return result;
+
+  } catch (error) {
+    if (localFilePath) {
+      fs.unlinkSync(localFilePath);
+    }
+    return null;
+  }
+}
+
+export {uploadOnCloudinary}
