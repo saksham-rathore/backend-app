8b27629e5c9ca683b71e370ae69a5bb492869188
saksham762007 <samrathore07062007@gmail.com>
2026-03-25 00:38:46 +0530

    file update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,6 @@
 import {asynchandler} from "../utils/asynchandler.js";
 import {ApiError} from "../utils/apierrors.js"
 
-
 const registerUser = asynchandler( async (req, res) => {
   const {fullname, email, username, password} = req.body
   console.log("email: ", email);
