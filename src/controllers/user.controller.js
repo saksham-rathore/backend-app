53bb1278effbc87da95454a425e6b00837342630
saksham762007 <samrathore07062007@gmail.com>
2026-03-24 23:52:59 +0530

    file update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,8 @@
 import {asynchandler} from "../../utils/asynchandler.js";
 
 const registerUser = asynchandler( async (req, res) => {
-    res.status(200).json({
-    message: "Ok"
-  })
+  const {fullname, email, username, password} = req.body
+  console.log("email: ", email);
 })
 
 export {registerUser}
