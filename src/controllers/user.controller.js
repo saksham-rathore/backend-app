1300608032beefcf1da8f441bef824adb1d37a78
saksham762007 <samrathore07062007@gmail.com>
2026-03-28 20:04:48 +0530

    feat: implement user authentication and profile management controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -247,6 +247,7 @@ const updateAccountDetails = asynchandler(async(req, res) => {
   return res.status(200).json(new ApiResponse(200, user, "Account details updated"))
 })
 
+
 const updateUserAvatar = asynchandler(async(req, res) => {
   const avatarLocalPath = req.file?.path
 
@@ -271,11 +272,40 @@ const updateUserAvatar = asynchandler(async(req, res) => {
   ).select("-password")
 
   return res.status(200).json(
-    new ApiResponse(200, user, "Avatar updated successfully")
+    new ApiResponse(200, User, "Avatar updated successfully")
+  )
+})
+
+
+const updateUserCoverImage = asynchandler(async(req, res) => {
+  const coverImageLocalPath = req.file?.path
+
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "CoverImage file is missing")
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if (!coverImage.url) {
+    throw new ApiError(400, "Error while uploading on CoverImage")
+  }
+
+  const User = await User.findByIdAndUpdate(
+    req.User?._id,
+    {
+      $set: {
+        coverImage: coverImage.url
+      }
+    },
+    {new: true}
+  ).select("-password")
+
+  return res.status(200).json(
+    new ApiResponse(200, User, "CoverImage updated successfully")
   )
 })
 
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser,
-  updateAccountDetails, updateUserAvatar
+  updateAccountDetails, updateUserAvatar, updateUserCoverImage
  };
