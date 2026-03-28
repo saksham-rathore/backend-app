c906acf41a510b20715877974f344da988a602c0
saksham762007 <samrathore07062007@gmail.com>
2026-03-28 19:30:29 +0530

    feat: implement user authentication and account management controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -227,5 +227,27 @@ const getCurrentUser = asynchandler(async(req, res) => {
   return res.status(200).json(200, req.user, "current user fetched successfully")
 })
 
+const updateAccountDetails = asynchandler(async(req, res) => {
+  const {fullname, email} = req.body
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser };
+  if (!fullname || !email) {
+    throw new ApiError(400, "All fields are required")
+  }
+
+  const user = User.findByIdAndUpdate(req.user?._id,
+    {
+      $set: {
+        fullname,
+        email
+      }
+    },
+    {new: true}
+  ).select("-password")
+
+  return res.status(200).json(new ApiResponse(200, user, "Account details updated"))
+})
+
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser,
+  updateAccountDetails
+ };
